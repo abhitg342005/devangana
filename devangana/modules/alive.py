4153a0a5f8a34041ea72c0f46d6ebca49b5f79b5
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-09-07 23:59:05 +0530

    Add files via upload

---
+++ devangana/modules/alive.py
@@ -0,0 +1,36 @@
+import os
+import re
+import random
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from devangana.events import register
+from devangana import telethn as tbot, SUPPORT_CHAT, OWNER_USERNAME, dispatcher
+
+
+PHOTO = [
+    "https://te.legra.ph/file/d4edce994b7a6512c4594.jpg",
+    "https://telegra.ph/file/1ae431a551b7e2e2339d9.jpg",
+]
+
+
+@register(pattern=("/alive"))
+async def awake(event):
+    TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ {dispatcher.bot.first_name}**\n━━━━━━━━━━━━━━━━━━━\n\n"
+    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [Krishna തുളസി](https://t.me/{OWNER_USERNAME})** \n\n"
+    TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
+    TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
+    TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
+    BUTTON = [
+        [
+            Button.url("ʜᴇʟᴘ​", f"https://t.me/{dispatcher.bot.username}?start=help"),
+            Button.url("sᴜᴘᴘᴏʀᴛ​", f"https://t.me/{SUPPORT_CHAT}"),
+        ]
+    ]
+    ran = random.choice(PHOTO)
+    await tbot.send_file(event.chat_id, ran, caption=TEXT, buttons=BUTTON)
+
+
+__mod_name__ = "Aʟɪᴠᴇ"
